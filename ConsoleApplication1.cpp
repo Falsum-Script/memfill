058ffc5f90d040a465cccc4c4745968b1ae2c497
Falsum-Script <82958786+Falsum-Script@users.noreply.github.com>
2021-09-29 18:20:59 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <malloc.h>
+#include < string.h >
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_OP 100000000
+#define TOO_MUCH_MEM 100000000
+int main(void)
+{
+    int cpt = 0;
+    for (int i = 0; i < MAX_OP; i++)
+    {
+        cpt++;
+    }
+    if (cpt == MAX_OP)
+    {
+        char* memdmp = NULL;
+        memdmp = (char*)malloc(TOO_MUCH_MEM);
+
+        if (memdmp != NULL)
+        {
+            memset(memdmp, 00, TOO_MUCH_MEM);
+            free(memdmp);
+        }
+        printf("DONE!\n");
+        return 0;
+    }
+}
